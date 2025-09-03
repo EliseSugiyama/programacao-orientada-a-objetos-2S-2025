5aaf1272766c8e7d079c89ca614f0237a901b9cb
EliseSugiyama <elise.sugiyama5@gmail.com>
2025-09-03 12:07:56 -0300

    second commit

--- sistema_radar/Radar.java
+++ sistema_radar/Radar.java
@@ -1,20 +1,31 @@
 
 
 public class Radar {
-    public Integer limitevelocidade;
-    public String modelo;
-    public String localizacao; 
-    public Integer velocidadeAvaliada;
+    private Integer limitevelocidade;
+    private String modelo;
+    private String localizacao; 
+    private Integer velocidadeObservada;
 
-    public void emitirNotificacao(String placa, Integer velocidadeAvaliada){
-        System.out.println("Limite de velocidade: " + this.limitevelocidade);
-        System.out.println("Velocidade avaliada: " + this.velocidadeAvaliada);
-        System.out.println("Placa: " + placa);
+    public Radar(Integer limitevelocidade, String modelo, String localizacao){
+        this.limitevelocidade = limitevelocidade;
+        this.modelo = modelo;
+        this.localizacao = localizacao;
     }
 
-    public void avaliarVelocidade(Carro c){
-        if (c.velocidade > this.limitevelocidade){
-            emitirNotificacao(c.placa, c.velocidade);
+    public Radar(Integer limitevelociade){
+        this.limitevelocidade = limitevelociade;
+    }
+
+    public void emitirNotificacao(String placa, Integer velocidadeObservada){
+        System.out.println("Placa: " + placa);
+        System.out.println("Velocidade observada: " + velocidadeObservada);
+        System.out.println("Localização: " + this.localizacao);
+        System.out.println("Velocidade da via: " + this.limitevelocidade);
+    }
+
+    public void avaliarVelocidade(Carro carro){
+        if (carro.getVelocidade() > this.limitevelocidade){
+            emitirNotificacao(carro.getPlaca(), carro.getVelocidade());
         }
     }
 }
--- sistema_radar/Simulacao.java
+++ sistema_radar/Simulacao.java
@@ -3,35 +3,21 @@
 public class Simulacao {
     
     public static void main(String[] args){
-        Carro opala = new Carro();
-        opala.velocidade = 0;
-        opala.placa = "ACLR300";
-        opala.modelo = "Comodoro";
-        opala.ano = 76;
+        Carro opala = new Carro("ACLR300", 0, 76, "Comodoro");
 
-        Radar radar = new Radar();
-        radar.modelo = "pardal";
-        radar.limitevelocidade = 60;
-        radar.localizacao = "Pistao Sul";
+        Radar radar = new Radar(60, "Pardal", "Pistao Sul");
         
+        radar.avaliarVelocidade(opala);
+
         opala.acelerar(); //10
         opala.acelerar(); //20
         opala.acelerar(); //30
         opala.acelerar(); //40
         opala.acelerar(); //50
         opala.acelerar(); //60
-
-        radar.avaliarVelocidade(opala);
-
         opala.acelerar(); //70
-
-        radar.avaliarVelocidade(opala);
-
-        opala.frear(); //60
-
+        
         radar.avaliarVelocidade(opala);
 
     }
-
-
 }
